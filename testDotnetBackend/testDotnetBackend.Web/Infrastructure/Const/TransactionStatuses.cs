590d0a2b28b2e335ddf50562dab5374e4a394669
Egor Yudin <eydnep2000@gmail.com>
2020-07-25 02:22:38 +0300

    Transaction consts added.

---
+++ testDotnetBackend/testDotnetBackend.Web/Infrastructure/Const/TransactionStatuses.cs
@@ -0,0 +1,9 @@
+﻿namespace testDotnetBackend.Web.Infrastructure.Const
+{
+    public static class TransactionStatuses
+    {
+        public const string PENDING = "Pending";
+        public const string COMPLETED = "Completed";
+        public const string CANCELLED = "Cancelled";
+    }
+}
---
+++ testDotnetBackend/testDotnetBackend.Web/Infrastructure/Const/TransactionTypes.cs
@@ -0,0 +1,8 @@
+﻿namespace testDotnetBackend.Web.Infrastructure.Const
+{
+    public static class TransactionTypes
+    {
+        public const string REFILL = "Refill";
+        public const string WITHDRAWAL = "Withdrawal";
+    }
+}

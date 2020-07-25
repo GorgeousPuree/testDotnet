1d70dd2205e23050cddf4ba64648622038a8a4c5
Egor Yudin <eydnep2000@gmail.com>
2020-07-26 02:03:13 +0300

    TransactionModelValidator added.

---
+++ testDotnetBackend/testDotnetBackend.Web/Abstractions/Validators/ITransactionModelValidator.cs
@@ -0,0 +1,9 @@
+﻿using testDotnetBackend.Web.Infrastructure.Responses;
+
+namespace testDotnetBackend.Web.Abstractions.Validators
+{
+    public interface ITransactionModelValidator
+    {
+        public OperationResult IsValid(string[] lineElements, out int transactionId, out decimal amount);
+    }
+}
---
+++ testDotnetBackend/testDotnetBackend.Web/Infrastructure/Validators/TransactionModelValidator.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using testDotnetBackend.Web.Abstractions.Validators;
+using testDotnetBackend.Web.Infrastructure.Const;
+using testDotnetBackend.Web.Infrastructure.Responses;
+
+namespace testDotnetBackend.Web.Infrastructure.Validators
+{
+    public class TransactionModelValidator : ITransactionModelValidator
+    {
+        public OperationResult IsValid(string[] lineElements, out int transactionId, out decimal amount)
+        {
+            transactionId = default;
+            amount = default;
+
+            if (!int.TryParse(lineElements[0], out transactionId))
+                return new OperationResult(false, "Cannot parse transaction id.");
+
+            if (!TransactionStatuses.ALL.Contains(lineElements[1]))
+                return new OperationResult(false, "Unknown transaction status.");
+
+            if (!TransactionTypes.ALL.Contains(lineElements[2]))
+                return new OperationResult(false, "Unknown transaction type.");
+
+            if (lineElements[3].Length > 150)
+                return new OperationResult(false, "Client name is too long.");
+
+            if (!decimal.TryParse(lineElements[4].Replace("$", "").Replace(".", ","), out amount))
+                return new OperationResult(false, "Cannot parse transaction amount.");
+
+            return new OperationResult(true);
+        }
+    }
+}

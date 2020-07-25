43f4855aeec851d98f596e27ab20c99e02c0c870
Egor Yudin <eydnep2000@gmail.com>
2020-07-26 02:04:08 +0300

    TransactionModel added.

---
+++ testDotnetBackend/testDotnetBackend.Web/Infrastructure/Models/TransactionModel.cs
@@ -0,0 +1,11 @@
+﻿namespace testDotnetBackend.Web.Infrastructure.Models
+{
+    public class TransactionModel
+    {
+        public int Id { get; set; }
+        public string Status { get; set; }
+        public string Type { get; set; }
+        public string ClientName { get; set; }
+        public decimal Amount { get; set; }
+    }
+}

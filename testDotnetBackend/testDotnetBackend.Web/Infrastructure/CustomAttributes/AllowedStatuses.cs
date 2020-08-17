787d8fae9ee8772081a71dc01e34c2ad08e3abdf
Egor Yudin <eydnep2000@gmail.com>
2020-08-18 01:27:24 +0300

    Custom attributes added.

---
+++ testDotnetBackend/testDotnetBackend.Web/Infrastructure/CustomAttributes/AllowedStatuses.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using testDotnetBackend.Web.Infrastructure.Const;
+
+namespace testDotnetBackend.Web.Infrastructure.CustomAttributes
+{
+    public class AllowedStatuses : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            List<string> statuses = value as List<string>;
+            if (statuses == null) return true;
+
+            foreach (var status in statuses)
+                if (!TransactionStatuses.ALL.Contains(status)) return false;
+
+            return true;
+        }
+    }
+}
---
+++ testDotnetBackend/testDotnetBackend.Web/Infrastructure/CustomAttributes/AllowedTypes.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using testDotnetBackend.Web.Infrastructure.Const;
+
+namespace testDotnetBackend.Web.Infrastructure.CustomAttributes
+{
+    public class AllowedTypes : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            List<string> types = value as List<string>;
+            if (types == null) return true;
+
+            foreach (var type in types)
+                if (!TransactionTypes.ALL.Contains(type)) return false;
+
+            return true;
+        }
+    }
+}

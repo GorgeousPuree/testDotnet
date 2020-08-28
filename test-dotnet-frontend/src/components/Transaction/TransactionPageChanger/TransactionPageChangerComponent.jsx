42cb7b1c530b549c3b209f9c42b28e0b9166d097
Egor Yudin <eydnep2000@gmail.com>
2020-08-28 23:23:30 +0300

    PageChanger amazing refactor.

--- test-dotnet-frontend/src/components/Transaction/TransactionPageChanger/TransactionPageChangerComponent.jsx
+++ test-dotnet-frontend/src/components/Transaction/TransactionPageChanger/TransactionPageChangerComponent.jsx
@@ -10,7 +10,6 @@ const TransactionPageChangerComponent = () => {
 	const pagesCount = useSelector(
 		(state) => state.TransactionReducer.pagesCount
 	);
-
 	const actualPageNumber = useSelector(
 		(state) => state.TransactionReducer.actualPageNumber
 	);
@@ -20,40 +19,102 @@ const TransactionPageChangerComponent = () => {
 		dispatch(TransactionActionCreators.setDesiredPageNumber(desiredPageNumber));
 	};
 
-	const paginationItems = [];
-	for (let i = 0; i < pagesCount; i++) {
-		if (actualPageNumber === i + 1)
-			paginationItems.push(
-				<Pagination.Item
-					active
-					key={i + 1}
-					onClick={() => changePage(i + 1)}
-					value={i + 1}
-				>
-					{i + 1}
-				</Pagination.Item>
-			);
-		else
-			paginationItems.push(
-				<Pagination.Item
-					key={i + 1}
-					onClick={() => changePage(i + 1)}
-					value={i + 1}
-				>
-					{i + 1}
-				</Pagination.Item>
-			);
-	}
+	// you'd probably don't want to look at this
+	const pagination = (c, m) => {
+		let current = c,
+			last = m,
+			delta = c === 1 || c === m ? 1 : 0,
+			left = current - delta,
+			right = current + delta + 1,
+			range = [],
+			rangeWithDots = [],
+			l,
+			isEllipsisOne = true;
+
+		for (let i = 1; i <= last; i++) {
+			if (i === 1 || i === last || (i >= left && i < right)) {
+				range.push(i);
+			}
+		}
+
+		for (let i of range) {
+			if (l) {
+				if (i - l === 2) {
+					if (l + 1 === actualPageNumber)
+						rangeWithDots.push(
+							<Pagination.Item
+								active
+								key={l + 1}
+								onClick={() => changePage(l + 1)}
+								value={i + 1}
+							>
+								{l + 1}
+							</Pagination.Item>
+						);
+					else
+						rangeWithDots.push(
+							<Pagination.Item
+								key={l + 1}
+								onClick={() => changePage(l + 1)}
+								value={l + 1}
+							>
+								{l + 1}
+							</Pagination.Item>
+						);
+				} else if (i - l !== 1) {
+					let previousElement =
+						range[range.findIndex((index) => index === i) - 1];
+					rangeWithDots.push(
+						isEllipsisOne ? (
+							<Pagination.Ellipsis
+								onClick={() => changePage(Math.ceil((i - previousElement) / 2))}
+							/>
+						) : (
+							<Pagination.Ellipsis
+								onClick={() =>
+									changePage(
+										previousElement + Math.ceil((i - previousElement) / 2)
+									)
+								}
+							/>
+						)
+					);
+					isEllipsisOne = false;
+				}
+			}
+			if (i === actualPageNumber)
+				rangeWithDots.push(
+					<Pagination.Item
+						active
+						key={i}
+						onClick={() => changePage(i)}
+						value={i}
+					>
+						{i}
+					</Pagination.Item>
+				);
+			else
+				rangeWithDots.push(
+					<Pagination.Item key={i} onClick={() => changePage(i)} value={i}>
+						{i}
+					</Pagination.Item>
+				);
+			l = i;
+		}
+		return rangeWithDots;
+	};
 
 	return (
 		<Pagination className={'justify-content-center'}>
 			<Pagination.Prev
+				disabled={actualPageNumber === 1}
 				onClick={() =>
 					actualPageNumber > 1 ? changePage(actualPageNumber - 1) : true
 				}
 			/>
-			{paginationItems}
+			{pagination(actualPageNumber, pagesCount)}
 			<Pagination.Next
+				disabled={actualPageNumber === pagesCount}
 				onClick={() =>
 					actualPageNumber < pagesCount
 						? changePage(actualPageNumber + 1)
